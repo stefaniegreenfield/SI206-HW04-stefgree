4f7eeee3eb67044dbd363893bc85175325eb6ac8
Stefanie Greenfield <stefgree@umich.edu>
2018-02-02 15:22:04 -0500

    new 2lines

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,5 @@
 def question():
-    q= input('What is your question?')
+    while q!='quit':
+        q=input('What is your question?')
+        if q[-1]!='?':
+            print('I’m sorry, I can only answer questions.')
